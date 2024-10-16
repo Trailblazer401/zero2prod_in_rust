f71cdc615bbbcde6e582cb5fe097c3a1d8078e52
Trailblazer401 <trailblazer401@outlook.com>
2024-10-16 17:17:09 +0800

    post test impl and using serde to impl deserialize

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -26,3 +26,48 @@ fn spawn_app() -> String {
 
     format!("http://127.0.0.1:{}", port)
 }
+
+#[tokio::test]
+async fn subscribe_returns_200_valid() {
+    let app_addr = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let reponse = client
+        .post(&format!("{}/subscriptions", &app_addr))
+        .header("Content-type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200,reponse.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_bad() {
+    let app_addr = spawn_app();
+    let client = reqwest::Client::new();
+    let test_case = vec![
+        ("name=le%20guin", "missing email"),
+        ("email=ursula_le_guin%40gmail.com", "missing name"),
+        ("", "missing both name and email")
+    ];
+    
+    for (invalid_body, error_msg) in test_case {
+        let reponse = client
+            .post(&format!("{}/subscriptions", &app_addr))
+            .header("Content-type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            reponse.status().as_u16(),
+            "The API did not fail with 400 on requst payload: \"{}\"",
+            error_msg
+        );
+    }
+}
