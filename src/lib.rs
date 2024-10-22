159af42ca7a19383449df6b51942078f8263758c
Trailblazer401 <trailblazer401@outlook.com>
2024-10-22 20:36:22 +0800

    use tracing for record logs

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,5 @@
 //! lib.rs
 
-// use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-// use std::net::TcpListener;
-
-// #[derive(serde::Deserialize)]    // 该处的属性宏#[derive()]用于自动为 FormData 结构体实现来自serde库的 trait: serde::Deserialize
-// struct FormData {
-//     email: String,
-//     name: String
-// }
-
-// async fn _greet(req: HttpRequest) -> impl Responder {    // greet返回一个实现了Responder trait的（任何）类型
-//     let name = req.match_info().get("name").unwrap_or("World");    // unwarp_or 从 Option 或 Result中提取值，返回 Some（Option）/ Ok（Result）中的值或 default （若结果为 None / Err），但不进行错误处理
-//     format!("Hello {}!", &name)
-// }
-
-// async fn health_check() -> HttpResponse {
-//     HttpResponse::Ok().finish()
-// }
-
-// async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-//     HttpResponse::Ok().finish()
-// }
-
-// #[tokio::main]    // 通过 tokio 宏使 main 函数在 tokio 提供的异步运行时上运行，由 tokio 宏负责补充样本代码
-// pub async fn run() -> std::io::Result<()> {    // 此处 Result<()> 中的（）为 Rust 中的 unit 类型，表示若 run 函数运行成功则返回一个Ok(())，仅通知运行成功但不返回有意义的数据
-// pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-//     let server = HttpServer::new(|| {
-//         App::new()
-//             // .route("/", web::get().to(greet))
-//             // .route("/{name}", web::get().to(greet))    // {name} 是一占位符，在客户端访问某URL路径时（如“/John”）匹配路径中的实际值。通过制定路由route("/{name}")，actix web在处理对应路由请求时会自动从路径中提取name参数，并通过web::get().to(greet)将对该路由的http get请求映射到处理函数greet实现参数传递，该参数传递由actix web通过函数签名自动推断完成，因此greet也没有显式的参数列表
-//             .route("/health_check", web::get().to(health_check))
-//             .route("/subscriptions", web::post().to(subscribe))
-//     })    // 此处 HttpServer::new(|| {...}) 中使用闭包进行参数传递，|...| 表示闭包的参数列表，该处没有传入闭包的参数，故参数列表为空（ || )，{...}表示闭包的实现体，包含闭包的执行逻辑，该闭包返回一个配置了路由的App实例
-//     .listen(listener)?    // 此处 ? 运算的对象是由bind函数运行返回的 Result<Self> 即 Result<HttpServer, E>，绑定成功则 Result<Self> 会是 Ok(HttpServer)，则该链式调用继续执行run方法；绑定失败则是 Err(std::io::Error)，则整个run函数在此处停止并返回该Err类型
-//     .run();
-//     // .await
-//     Ok(server)
-// }
-
 pub mod configurations;
 pub mod routes;
 pub mod startup;
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,17 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let requset_id = Uuid::new_v4();
+    // tracing::info!("Request id:{} - Adding '{}:{}' as a new subscriber...", requset_id, form.name, form.email);
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber...",
+        %requset_id,
+        subscriber_name = %form.name,
+        subscriber_email = %form.email
+    );
+    let _request_span_guard = request_span.enter();
+
+    tracing::info!("Request id:{} - Saving new subscriber details into database...", requset_id);
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,9 +34,13 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.get_ref())    // 若 subscribe 函数保留 PgConnection 作为参数，则不满足此处execute方法要求参数实现 Executor trait，PgConnection类型的可变引用实现了该 trait（可变引用的唯一性保证同时只能存在一个在该Postgres连接上的查询），但 web::Data 无法提供对原类型的可变引用
     .await    // 使用PgPool类型通过内部可变性实现共享引用
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("Request id:{} - New subscriber details have been saved successfully.", requset_id);
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            // println!("Failed to execute query: {}", e);
+            tracing::error!("Request id:{} - Failed to execute query: {:?}", requset_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
